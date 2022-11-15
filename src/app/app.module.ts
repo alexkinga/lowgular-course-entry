44dce76aa621f6e220f9a30b705b5b9635daee7b
Kinga Aleksiejczuk <kinga.aleksiejczuk28@gmail.com>
2022-11-15 23:15:29 +0100

    feat: added new module || new service || exam task done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
 import {EmployeesFacesComponentModule} from "./ui/employees-faces/employees-faces.component-module";
 import {EmployeeServiceModule} from "./services/employee.service-module";
+import {ProjectServiceModule} from "./services/project.service-module";
+import {ProjectListComponent} from "./ui/project-list/project-list.component";
+import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +18,9 @@ import {EmployeeServiceModule} from "./services/employee.service-module";
     HttpClientModule,
     EmployeeListComponentModule,
     EmployeeServiceModule,
+    ProjectServiceModule,
     EmployeesFacesComponentModule,
+    ProjectListComponentModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/ui/project-list/project-list.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {Observable} from "rxjs";
+import {ProjectModel} from "../../model/project.model";
+import {ProjectService} from "../../services/project.service";
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProjectListComponent {
+  title: string = 'Projects';
+  data$: Observable<ProjectModel[]> = this._projectService.getAll();
+  constructor(private _projectService: ProjectService) { }
+}
