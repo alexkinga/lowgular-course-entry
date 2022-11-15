da2168be0faeabb14555677110facb8f3ae3eb07
Kinga Aleksiejczuk <kinga.aleksiejczuk28@gmail.com>
2022-11-15 22:24:02 +0100

    feat: added new module || Task 2 exercise 4

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,8 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { EmployeeService } from '../../services/employee.service';
-import { EmployeeModel } from '../../model/employee.model';
+import {PersonModel} from "../../model/person.model";
 
 @Component({
   selector: 'employee-list',
@@ -12,6 +11,6 @@ import { EmployeeModel } from '../../model/employee.model';
 })
 export class EmployeeListComponent {
   title: string = 'My title';
-  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
+  data$: Observable<PersonModel[] | null> = this._employeeService.getAll();
   constructor(private _employeeService: EmployeeService) { }
 }
--- src/app/ui/employees-faces/employees-faces.component.ts
+++ src/app/ui/employees-faces/employees-faces.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from "rxjs";
-import {EmployeeModel} from "../../model/employee.model";
 import {EmployeeService} from "../../services/employee.service";
+import {PersonModel} from "../../model/person.model";
 
 @Component({
   selector: 'app-employees-faces',
@@ -11,6 +11,6 @@ import {EmployeeService} from "../../services/employee.service";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EmployeesFacesComponent {
-  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
+  data$: Observable<PersonModel[] | null> = this._employeeService.getAll();
   constructor(private _employeeService: EmployeeService) { }
 }
