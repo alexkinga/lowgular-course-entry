a931901475e127ace1bea2724eb6744e163f8e5a
Kinga Aleksiejczuk <kinga.aleksiejczuk28@gmail.com>
2022-11-17 00:35:37 +0100

    feat: TASK 3 || Response || Ex 5

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { PersonModel } from '../model/person.model';
+import { ApiResponse } from './api.response';
+import { EmployeeResponse } from './employee.response';
 import { CreateEmployeeModel } from '../model/create-employee.model';
 
 @Injectable()
@@ -9,7 +12,19 @@ export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
   getAll(): Observable<PersonModel[]> {
-    return this._httpClient.get<PersonModel[]>('assets/data/people.json');
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/employees').pipe(
+      map((response : ApiResponse<EmployeeResponse[]>): PersonModel[] => {
+        return response.data.map((employeeResponse : EmployeeResponse) => {
+          return {
+            name: employeeResponse.employee_name,
+            age: employeeResponse.employee_age,
+            personalNumber: employeeResponse.id,
+            img: employeeResponse.profile_image,
+            surname: '',
+            mail: employeeResponse.employee_name + '@lowgular.io'
+          }
+        });
+      }));
   }
 
   //created with lowgular
