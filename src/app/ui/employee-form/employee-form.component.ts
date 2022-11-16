0036a67eabbb8f9a723355a8b0657f51d50f1b23
Kinga Aleksiejczuk <kinga.aleksiejczuk28@gmail.com>
2022-11-16 21:40:11 +0100

    feat: TASK 3 || Create Operation || Ex 3 + task for volunteers

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import { CreateEmployeeModel } from '../../model/create-employee.model';
 
 @Component({
   selector: 'app-employee-form',
@@ -9,13 +11,24 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class EmployeeFormComponent {
   // FormGroup śledzi stan wartości i poprawność w całym formularzu, formControl - pojedynczej kontrolki
+
   readonly employeeForm: FormGroup = new FormGroup({
     name: new FormControl(null, [Validators.required]),
     age: new FormControl(null, [Validators.min(0)]),
     salary: new FormControl(null, [Validators.required, Validators.min(0)]),
   });
 
-  onButtonClicked(form: {name:string, age:number, salary:number}){
-    alert('User was successfully added to the database. ' + 'Name: ' + form.name + ', Age: ' +  form.age + ', Salary: ' + form.salary);
+  onButtonClicked(form: { name: string, age: number, salary: number }) {
+    alert('User was successfully added to the database. ' + 'Name: ' + form.name + ', Age: ' + form.age + ', Salary: ' + form.salary);
+  }
+
+  constructor(private _employeeService: EmployeeService) { }
+
+//created with lowgular
+  onFormSubmitted(form: CreateEmployeeModel) {
+    this._employeeService.create({ name: form.name, age: form.age, salary: form.salary }).subscribe(() => {
+      alert('User was successfully added to the database. ' + 'Name: ' + form.name + ', Age: ' + form.age + ', Salary: ' + form.salary);
+    });
+
   }
 }
